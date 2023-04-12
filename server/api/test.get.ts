496fbebdd71a3f5892b248375d6327a35308e19c
Dario Piotrowicz <dario@cloudflare.com>
2023-04-12 11:16:11 +0100

    add typeOfwaitUntil and requestStr to api test response

--- server/api/test.get.ts
+++ server/api/test.get.ts
@@ -1,4 +1,6 @@
 export default defineEventHandler((event) => {
     const context = event.context;
-    return { context };
+    const typeOfWaitUntil = typeof (context as any)?.cloudflare?.context?.waitUntil;
+    const requestStr = JSON.stringify((context as any)?.cloudflare?.request);
+    return { context, typeOfWaitUntil, requestStr };
 });
